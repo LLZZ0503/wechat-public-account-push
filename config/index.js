79570a1c27dde870f9aecb48483a4472c4eb2dad
LLZZ0503 <112355263+LLZZ0503@users.noreply.github.com>
2022-08-28 18:37:22 -0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -53,8 +53,8 @@ export const config = {
 // 天气：{{weather.DATA}}  
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
-// 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
+// 今天是我们遇见的第{{love_day.DATA}}天
+// 离我们见面还有{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
